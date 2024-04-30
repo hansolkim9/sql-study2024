63927e8ea9bf576fde33c35d0d20f909e6a8cebe
hansolkim9 <masicer2@gmail.com>
2024-04-30 09:23:54 +0900

    sqld DML WHERE

---
+++ 2. DML 기초 학습.sql
@@ -0,0 +1,121 @@
+CREATE TABLE goods (
+    id NUMBER(6) PRIMARY KEY,
+    goods_name VARCHAR2 (10) NOT NULL,
+    price NUMBER(10) DEFAULT 1000,
+    reg_date DATE
+);
+
+-- INSERT
+INSERT INTO goods
+    (id, goods_name, price, reg_date)
+VALUES
+    (1, '선풍기', 120000, SYSDATE);
+    
+INSERT INTO goods
+    (id, goods_name, price, reg_date)
+VALUES
+    (2, '세탁기', 200000, SYSDATE);
+
+INSERT INTO goods
+    (id, goods_name, reg_date)
+VALUES
+    (3, '달고나', SYSDATE);
+    
+INSERT INTO goods
+    (id, goods_name)
+VALUES
+    (4, '계란');
+    
+INSERT INTO goods
+    (goods_name, id, reg_date, price)
+VALUES
+    ('점퍼', 5, SYSDATE, '49000');
+
+-- 컬럼명 생략시 테이블구조 순서대로 자동 기입 (되도록 기입해 두는걸 권장)
+INSERT INTO goods
+    (goods_name, id, reg_date, price)
+VALUES
+    (6, '냉장고', 1000000, SYSDATE);
+
+-- UPDATE
+UPDATE goods
+SET goods_name = '에어컨'
+WHERE id = 1
+;
+
+-- WHERE 없으면 전체 수정해버림
+UPDATE goods
+SET price = 9999;
+
+UPDATE goods
+SET id = 11
+WHERE id = 4;
+
+UPDATE goods
+SET price = null
+WHERE id = 3;
+
+UPDATE goods
+SET goods_name = '청바지', price = 299000
+WHERE id = 3;
+
+-- DELETE
+
+DELETE FROM goods
+WHERE id = 11;
+
+-- 조건 없이 delete하면 전체삭제됨 (다만 이 문법은 복구 가능)
+DELETE FROM goods;
+TRUNCATE TABLE goods; -- 복구 불가, 테이블 생성 초기로 돌아가는 것(DDL)
+DROP TABLE goods; -- 복구불가 + 테이블도 날라감
+
+DELETE FROM goods
+WHERE price > 1000;
+
+
+-- 조회
+SELECT * FROM goods;
+
+-- SELECT 기본 
+SELECT
+    certi_cd, certi_nm, issue_insti_nm
+FROM tb_certi;
+
+SELECT
+    certi_nm, issue_insti_nm
+FROM tb_certi;
+
+-- (ALL 기본으로 생략되어있음)
+SELECT ALL
+    certi_nm, issue_insti_nm
+FROM tb_certi;
+
+-- 모든 컬럼 조회 (수정 및 유지보수 불편해서 사용을 권장하진 않음)
+SELECT
+    *
+FROM tb_certi;
+
+-- 열 별칭 부여
+SELECT
+    emp_nm 사원명,
+    addr "거주지 주소"
+FROM tb_emp
+;
+
+-- 문자열 결합하기
+SELECT
+    '자격증: ' || certi_nm AS "자격증 정보"
+FROM tb_certi;
+
+SELECT
+    certi_nm || ' (' || issue_insti_nm || ')' AS "자격증"
+FROM tb_certi;
+
+
+
+
+
+
+
+
+
